b5b586adb959394bcdaacf49a00809b757afa047
Suparada Saitalae <ann@Suparadas-MacBook-Pro.local>
2020-03-10 17:07:48 -0500

    sock

--- sock.js
+++ sock.js
@@ -2,11 +2,27 @@
 
 function sockMerchant(n, ar) {
     let pairCount = 0
-    for (let i; i <= ar.length; i++){
-        console.log(i)
+    let pairTrack = {}
+    for (let i = 0; i < ar.length; i++){
+       pairTrack[ar[i]] = (pairTrack[ar[i]] + 1) || 1 
+        console.log(pairTrack)
+    }
+    for (value in pairTrack){
+        pairCount += Math.floor(pairTrack[value] / 2)
     }
     
+    return pairCount
 
     
 }
-console.log('hi')
+const n = 100
+const ar = [ 10, 20, 20, 10, 10, 30, 50, 10, 20 ]
+const ar2 = [
+    42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,
+    42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,
+    42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,
+    42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,
+    42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,
+]
+console.log(sockMerchant(n, ar))
+// console.log(Math.floor(1 / 2))
