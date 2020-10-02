9fece94edd85b2575d2f6b5c217fd395fa51fe15
Ann <ann.suparada@gmail.com>
2020-10-02 09:59:56 -0500

    clean up

--- easy/pageCount.js
+++ easy/pageCount.js
@@ -16,13 +16,9 @@ function pageCount(n, p) {
   if (p === n || p === 1) return 0;
   console.log("\n");
   if (p < half) {
-    console.log("start");
     return Math.floor(p / 2);
   } else {
-    console.log("last");
     if (n % 2 === 1) {
-      console.log("kee");
-      //   if (turning % 2 === 0) return turning - 1;
       return Math.floor(turning);
     }
     if (n % 2 === 0) {
@@ -42,4 +38,3 @@ console.log("test 5:", pageCount(7, 3)); //1
 console.log("test 6:", pageCount(6, 3)); //1
 console.log("test 7:", pageCount(37455, 29835)); //3810
 console.log("test 8:", pageCount(95073, 17440)); //8720
-// console.log(Math.floor(70.5));
