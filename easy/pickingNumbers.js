57860c0510b16224b9c0657b162f175e40f56383
Ann <ann.suparada@gmail.com>
2020-11-17 16:25:05 -0600

    a very big sum

--- easy/pickingNumbers.js
+++ easy/pickingNumbers.js
@@ -1,3 +1,20 @@
+// Given an array of integers, find the longest subarray where the absolute difference between any two elements is less than or equal to .
+
+// Example
+
+// There are two subarrays meeting the criterion:  and . The maximum length subarray has  elements.
+
+// Function Description
+
+// Complete the pickingNumbers function in the editor below.
+
+// pickingNumbers has the following parameter(s):
+
+// int a[n]: an array of integers
+// Returns
+
+// int: the length of the longest subarray that meets the criterion
+
 function pickingNumbers(a) {
   let obj = {};
   let longestLength = 0;
