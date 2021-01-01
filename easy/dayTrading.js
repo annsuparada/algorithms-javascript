73ab501caf00c6bb6f4cca3493aea8cba56c2b76
Ann <ann.suparada@gmail.com>
2021-01-01 15:58:08 -0600

    day trading

--- easy/dayTrading.js
+++ easy/dayTrading.js
@@ -4,13 +4,33 @@ function dayTrading(capital, percentGain, days) {
     let temp = (balance * percentGain) / 100;
     balance = balance + temp;
   }
+
   let profit = balance - capital;
   let result = {
     capital: capital,
     balance: Number(balance.toFixed(2)),
     profit: Number(profit.toFixed(2)),
   };
+
   return result;
 }
 
-console.log(dayTrading(6000, 2, 20));
+function detailDayTrading(capital, percentGain, days) {
+  let balance = capital;
+  let result = {};
+  for (let i = 0; i < days; i++) {
+    let temp = (balance * percentGain) / 100;
+    balance = balance + temp;
+
+    let profit = balance - capital;
+    result[i + 1] = {
+      balance: Number(balance.toFixed(2)),
+      profit: Number(profit.toFixed(2)),
+    };
+  }
+
+  return result;
+}
+
+// console.log(dayTrading(4000, 1.5, 20));
+console.log(detailDayTrading(6000, 1.5, 480));
